c22f2a23f9df18b111c9ffe0678d1d87acbab346
“Elka74100 <>
2020-03-08 18:02:24 +0100

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -59,8 +59,6 @@ class Main extends Component {
     return (
       <div>
         <Header/>
-        {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
